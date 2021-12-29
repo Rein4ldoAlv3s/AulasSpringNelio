93337f00d21c3e0c99841586a352d284360bdddf
Rein4ldoAlv3s <naldos15@hotmail.com>
2021-12-29 19:23:50 -0300

    listar um cad funciona

--- src/main/java/com/reinaldo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/reinaldo/cursomc/resources/CategoriaResource.java
@@ -3,25 +3,28 @@ package com.reinaldo.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.reinaldo.cursomc.domain.Categoria;
+import com.reinaldo.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok(obj);
 	}
 }
