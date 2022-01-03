7fbb3a628dfa7dfe5dd41d824582a47027a494f5
Rein4ldoAlv3s <naldos15@hotmail.com>
2022-01-03 18:42:55 -0300

    C:/Program Files/Git/clientes/id funciona

---
+++ src/main/java/com/reinaldo/cursomc/resources/ClienteResource.java
@@ -0,0 +1,32 @@
+package com.reinaldo.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reinaldo.cursomc.domain.Cliente;
+import com.reinaldo.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok(obj);
+	}
+	
+	
+}
